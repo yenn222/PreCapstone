1e9c8eeabe26507ac2e12480ffd5384c8b26a69a
r3pic <101osc@naver.com>
2024-08-13 02:23:52 +0900

    refactor: app 모듈 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ArticleModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
