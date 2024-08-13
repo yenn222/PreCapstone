0c867438c29e1d154b1b84df881ce87493c8aeed
r3pic <101osc@naver.com>
2024-08-13 18:15:48 +0900

    refactor: 게시글 생성, 조회 기능 구현
    api 명세에 맞게 변경함

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,29 +1,36 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Res, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Res, ParseIntPipe, Query, NotFoundException } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { Response } from 'express';
 import { GetArticleResponseDto } from './dto/get-article.response.dto';
-import { GetArticlesResponseDto } from './dto/get-articles.response.dto';
+import { GetArticleListResponseDto } from './dto/get-article-list.response.dto';
 import { CreateArticleResponseDto } from './dto/create-article.response.dto';
+import { CreateArticleRequestDto } from './dto/create-article.request.dto';
 
 @Controller('article')
 export class ArticleController {
-  constructor(private readonly articleService: ArticleService) {}
+  constructor(private readonly articleService: ArticleService) { }
 
   @Post()
-  createArticle(@Body() createArticleDto, @Res() res: Response) {
-    const article: CreateArticleResponseDto = this.articleService.createArticle(createArticleDto);
+  create(@Body() createArticleDto: CreateArticleRequestDto, @Res() res: Response) {
+    const article: CreateArticleResponseDto = this.articleService.create('이혜현', createArticleDto); // author는 하드코딩
+
     res.status(HttpStatus.CREATED).json(article);
   }
 
   @Get()
-  getAllArticles(@Res() res: Response) {
-    const articles: GetArticlesResponseDto[] = this.articleService.getAllArticles();
-    res.status(HttpStatus.OK).json({ articles: articles });
+  getFromPage(@Query('page', new ParseIntPipe()) page: number, @Query('perpage', new ParseIntPipe()) perPage: number, @Res() res: Response) {
+    const articles: GetArticleListResponseDto[] = this.articleService.getFromPage(page, perPage);
+
+    res.status(HttpStatus.OK).json({
+      maxPage: this.articleService.getLastPage(perPage),
+      articles: articles
+    });
   }
 
   @Get(':id')
-  getArticle(@Param('id', new ParseIntPipe()) articleid: number, @Res() res: Response) {
-    const article: GetArticleResponseDto = this.articleService.getArticle(articleid);
+  getbyId(@Param('id', new ParseIntPipe()) articleid: number, @Res() res: Response) {
+    const article: GetArticleResponseDto = this.articleService.getbyId(articleid);
+
     res.status(HttpStatus.OK).json(article);
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,27 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { CreateArticleRequestDto } from './dto/create-article.request.dto';
 import { db } from '../database/arrayDB';
 import { GetArticleResponseDto } from './dto/get-article.response.dto';
 import { ArticleEntity } from './entity/article.entity';
-import { GetArticlesResponseDto } from './dto/get-articles.response.dto';
+import { GetArticleListResponseDto } from './dto/get-article-list.response.dto';
 import { CreateArticleResponseDto } from './dto/create-article.response.dto';
 
 @Injectable()
 export class ArticleService {
-  createArticle(dto: CreateArticleRequestDto): CreateArticleResponseDto {
-    const articleEntity: ArticleEntity = db.add(dto);
+  /**
+   * 게시글을 생성합니다.
+   * 
+   * @param author 게시글 작성자
+   * @param dto 게시글 생성 요청 데이터
+   * @returns 생성된 게시글의 정보
+  */
+  create(author: string, dto: CreateArticleRequestDto): CreateArticleResponseDto {
+    const articleEntity: ArticleEntity = db.add(author, dto);
     const articleResponseDto: CreateArticleResponseDto = {
       id: articleEntity.id,
       title: articleEntity.title,
       content: articleEntity.content,
       author: articleEntity.author,
       created_at: articleEntity.created_at,
-    };
+    }
     return articleResponseDto;
   }
 
-  getArticle(id: number): GetArticleResponseDto {
-    const articleEntity: ArticleEntity = db.get(id);
+  /**
+   * 게시글을 조회합니다.
+   * 
+   * @param id 조회할 게시글의 ID
+   * @returns 조회된 게시글의 정보
+   * @throws NotFoundException 게시글이 존재하지 않을 때
+  */
+  getbyId(id: number): GetArticleResponseDto {
+    const articleEntity: ArticleEntity = db.findById(id);
+    if (!articleEntity) {
+      throw new NotFoundException('게시글이 존재하지 않습니다.');
+    }
     const articleResponseDto: GetArticleResponseDto = {
       id: articleEntity.id,
       title: articleEntity.title,
@@ -29,21 +46,79 @@ export class ArticleService {
       author: articleEntity.author,
       created_at: articleEntity.created_at,
       updated_at: articleEntity.updated_at,
-    };
+    }
     return articleResponseDto;
   }
 
-  getAllArticles(): GetArticlesResponseDto[] {
+  /**
+   * 모든 게시글을 조회합니다.
+   * 
+   * @returns 모든 게시글의 정보
+   * @throws NotFoundException 게시글이 존재하지 않을 때
+  */
+  getAll(): GetArticleListResponseDto[] {
     const allArticles = db.getAll();
+    if (allArticles.length <= 0) {
+      throw new NotFoundException('게시글이 존재하지 않습니다.');
+    }
     return allArticles.map((articleEntity) => {
-      return {
+      const dto: GetArticleListResponseDto = {
         id: articleEntity.id,
         title: articleEntity.title,
-        auther: articleEntity.author,
+        author: articleEntity.author,
         fixed: articleEntity.fixed,
         created_at: articleEntity.created_at,
         updated_at: articleEntity.updated_at,
-      };
+      }
+      return dto;
     });
+  }
+
+  /**
+   * 페이지 범위에 해당하는 게시글을 조회합니다.
+   * 
+   * @param page 조회할 페이지
+   * @param perPage 페이지 당 게시글 수
+   * @returns 페이지 범위에 해당하는 게시글의 정보
+   * @throws NotFoundException 게시글이 존재하지 않을 때
+   * @throws UnprocessableEntityException 페이지 범위를 초과했을 때
+  */
+  getFromPage(page: number, perPage: number): GetArticleListResponseDto[] {
+    const allArticles: ArticleEntity[] = db.getAll();
+    if (allArticles.length <= 0) {
+      throw new NotFoundException('게시글이 존재하지 않습니다.');
+    }
+    const start = (page - 1) * perPage;
+    const end = start + perPage;
+    if (start > allArticles.length) {
+      throw new UnprocessableEntityException('페이지 범위를 초과했습니다.');
+    }
+
+    const articles = allArticles.slice(start, end);
+    return articles.map((articleEntity) => {
+      const dto: GetArticleListResponseDto = {
+        id: articleEntity.id,
+        title: articleEntity.title,
+        author: articleEntity.author,
+        fixed: articleEntity.fixed,
+        created_at: articleEntity.created_at,
+        updated_at: articleEntity.updated_at,
+      }
+      return dto;
+    });
+  }
+
+  /**
+   * 게시글의 마지막 페이지를 조회합니다.
+   * 
+   * @param perPage 페이지 당 게시글 수
+   * @returns 게시글의 마지막 페이지 번호
+   */
+  getLastPage(perPage: number): number {
+    const allArticles: ArticleEntity[] = db.getAll();
+    if (allArticles.length <= 0) {
+      return 0;
+    }
+    return Math.ceil(allArticles.length / perPage);
   }
 }
