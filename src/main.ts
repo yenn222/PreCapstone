c2f8a9ec8122acfc3494abbeb9879147eade3193
r3pic <101osc@naver.com>
2024-08-13 17:44:36 +0900

    feat: Validation 파이프 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { db } from './database/arrayDB';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+  db.dummyInit();
   await app.listen(3000);
 }
 bootstrap();
